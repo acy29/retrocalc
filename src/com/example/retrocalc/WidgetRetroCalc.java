4e6cdd07b0f4ccd8a93d0a130c59665019c33224
acy29 <andres29111990@gmail.com>
2013-06-24 18:36:52 -0430

    base

---
+++ src/com/example/retrocalc/WidgetRetroCalc.java
@@ -0,0 +1,14 @@
+package com.example.retrocalc;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class WidgetRetroCalc extends AppWidgetProvider{
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		// Actualizar el widget
+		// ...
+	}
+}
